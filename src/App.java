8dc5b1d3e86817861ebe62fda40e175551011a3d
Jeff Lowy <jeffreylowy@gmail.com>
2021-08-07 20:03:28 -0700

    Custom objects and comment out cloud function.

--- src/App.java
+++ src/App.java
@@ -10,9 +10,31 @@ class App {
         System.out.println("New age is " + studentAge + " years old.");
         letsTryThis(studentAge);
         input.close();
+        looper();
     }
 
     public static void letsTryThis(int age) {
         System.out.println("Hey, its me from the other method. Anyway, here's the age again: " + age);
+    }
+
+    public static void looper() {
+        Animal jeff = new Animal("Jeff", 38);
+        Animal vanessa = new Animal("Vanessa", 35);
+        Animal kali = new Animal("Kali", 10);
+        Animal chico = new Animal("Chico", 14);
+        Animal[] names = { jeff, vanessa, kali, chico };
+        System.out.println(names[0].name + " lives with " + names[1].name + " and " + names[2].name);
+        System.out.println("The " + names.length + " of them live in California.");
+        System.out.println("RIP my guy, " + names[3].name);
+    }
+}
+
+class Animal {
+    String name = "";
+    int age = 0;
+
+    Animal(String name, int age) {
+        this.name = name;
+        this.age = age;
     }
 }
--- src/CloudFunction.java
+++ src/CloudFunction.java
@@ -1,15 +1,17 @@
-package com.example;
+// package com.example;
 
-import com.google.cloud.functions.HttpFunction;
-import com.google.cloud.functions.HttpRequest;
-import com.google.cloud.functions.HttpResponse;
-import java.io.BufferedWriter;
+// import com.google.cloud.functions.HttpFunction;
+// import com.google.cloud.functions.HttpRequest;
+// import com.google.cloud.functions.HttpResponse;
+// import java.io.BufferedWriter;
 
-public class Example implements HttpFunction {
-	@Override
-	public void service(HttpRequest request, HttpResponse response) throws Exception {
-		BufferedWriter writer = response.getWriter();
-		writer.write(
-				"<html><head><title>test</title></head><body><img src='https://i1.sndcdn.com/avatars-000689822399-n8zm5q-t240x240.jpg'/></body></html>");
-	}
-}
+// public class Example implements HttpFunction {
+// @Override
+// public void service(HttpRequest request, HttpResponse response) throws
+// Exception {
+// BufferedWriter writer = response.getWriter();
+// writer.write(
+// "<html><head><title>test</title></head><body><img
+// src='https://i1.sndcdn.com/avatars-000689822399-n8zm5q-t240x240.jpg'/></body></html>");
+// }
+// }
