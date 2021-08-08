63aa7dec726a55c88b213231edd840dd62e3265f
Jeff Lowy <jeffreylowy@gmail.com>
2021-08-07 17:45:51 -0700

    Moved code from repl to vscode

--- src/App.java
+++ src/App.java
@@ -1,5 +1,18 @@
-public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+import java.util.Scanner;
+
+// Primitive types
+class Main {
+    public static void main(String[] args) {
+        int studentAge = 15;
+        System.out.println("Current age is " + studentAge + ". What's the new age?");
+        Scanner input = new Scanner(System.in);
+        studentAge = input.nextInt();
+        System.out.println("New age is " + studentAge + " years old.");
+        letsTryThis(studentAge);
+        input.close();
+    }
+
+    public static void letsTryThis(int age) {
+        System.out.println("Hey, its me from the other method. Anyway, here's the age again: " + age);
     }
 }
