da9c4a92fcd9b7c9754dbc0bcec5c14310f55745
Jeffrey Lowy <jeffreylowy@gmail.com>
2021-08-13 23:50:44 -0700

    Simple Generics

--- src/Loops.java
+++ src/Loops.java
@@ -8,6 +8,26 @@ class Loopy {
 			x = x + 1;
 		}
 
+		Car a = new Car("Jeff");
+		CustomObject<Car> obj = new CustomObject(a);
+
 		System.out.println("This is after the loop");
+		System.out.println("Value of x is " + obj.x.name);
+	}
+}
+
+class Car {
+	public String name;
+	
+	public Car(String name) {
+		this.name = name;
+	}
+}
+
+class CustomObject<T> {
+	public T x;
+
+	public CustomObject(T x) {
+		this.x = x;
 	}
 }
